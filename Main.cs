d8a202713cd541a601f04940b4b3ae65236da48b
starlying <starlying@gmail.com>
2018-05-31 09:14:24 +0800

    plugin1.7

--- Main.cs
+++ Main.cs
@@ -1,19 +1,80 @@
-﻿using SiteServer.Plugin;
-using SS.Jobs.Provider;
+﻿using System.Collections.Generic;
+using SiteServer.Plugin;
 
 namespace SS.Jobs
 {
     public class Main : PluginBase
     {
-        public static Main Instance { get; private set; }
+        private const string Department = "Department";
+        private const string Location = "Location";
+        private const string NumberOfPeople = "NumberOfPeople";
+        private const string Responsibility = "Responsibility";
+        private const string Requirement = "Requirement";
 
         public override void Startup(IService service)
         {
-            Instance = this;
-
-            service
-                .AddContentModel(JobDao.TableName, JobDao.TableColumns)
-                ;
+            service.AddContentModel("ss_jobs", new List<TableColumn>
+            {
+                new TableColumn
+                {
+                    AttributeName = Department,
+                    DataType = DataType.VarChar,
+                    DataLength = 200,
+                    InputStyle = new InputStyle
+                    {
+                        InputType = InputType.Text,
+                        DisplayName = "所属部门",
+                        IsRequired = true
+                    }
+                },
+                new TableColumn
+                {
+                    AttributeName = Location,
+                    DataType = DataType.VarChar,
+                    DataLength = 200,
+                    InputStyle = new InputStyle
+                    {
+                        InputType = InputType.Text,
+                        DisplayName = "工作地点",
+                        IsRequired = true
+                    }
+                },
+                new TableColumn
+                {
+                    AttributeName = NumberOfPeople,
+                    DataType = DataType.VarChar,
+                    DataLength = 200,
+                    InputStyle = new InputStyle
+                    {
+                        InputType = InputType.Text,
+                        DisplayName = "招聘人数",
+                        IsRequired = true,
+                        DefaultValue = "不限"
+                    }
+                },
+                new TableColumn
+                {
+                    AttributeName = Responsibility,
+                    DataType = DataType.Text,
+                    InputStyle = new InputStyle
+                    {
+                        InputType = InputType.TextEditor,
+                        DisplayName = "工作职责",
+                        IsRequired = true
+                    }
+                },
+                new TableColumn
+                {
+                    AttributeName = Requirement,
+                    DataType = DataType.Text,
+                    InputStyle = new InputStyle
+                    {
+                        InputType = InputType.TextEditor,
+                        DisplayName = "工作要求",
+                        IsRequired = true
+                    }
+                }
+            });
         }
     }
 }
