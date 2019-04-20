c7096ca97615dac3c3e4ca6707789ed798360fcc
starlying <starlying@gmail.com>
2019-04-20 22:20:48 +0800

    jobs

--- Main.cs
+++ Main.cs
@@ -15,69 +15,70 @@ namespace SS.Jobs
         public override void Startup(IService service)
         {
             service.AddContentModel("ss_jobs", new List<TableColumn>
-            {
-                new TableColumn
                 {
-                    AttributeName = Department,
-                    DataType = DataType.VarChar,
-                    DataLength = 200
+                    new TableColumn
+                    {
+                        AttributeName = Department,
+                        DataType = DataType.VarChar,
+                        DataLength = 200
+                    },
+                    new TableColumn
+                    {
+                        AttributeName = Location,
+                        DataType = DataType.VarChar,
+                        DataLength = 200
+                    },
+                    new TableColumn
+                    {
+                        AttributeName = NumberOfPeople,
+                        DataType = DataType.VarChar,
+                        DataLength = 200
+                    },
+                    new TableColumn
+                    {
+                        AttributeName = Responsibility,
+                        DataType = DataType.Text
+                    },
+                    new TableColumn
+                    {
+                        AttributeName = Requirement,
+                        DataType = DataType.Text
+                    }
                 },
-                new TableColumn
+                new List<InputStyle>
                 {
-                    AttributeName = Location,
-                    DataType = DataType.VarChar,
-                    DataLength = 200
-                },
-                new TableColumn
-                {
-                    AttributeName = NumberOfPeople,
-                    DataType = DataType.VarChar,
-                    DataLength = 200
-                },
-                new TableColumn
-                {
-                    AttributeName = Responsibility,
-                    DataType = DataType.Text
-                },
-                new TableColumn
-                {
-                    AttributeName = Requirement,
-                    DataType = DataType.Text
-                }
-            }, new List<InputStyle>
-            {
-                new InputStyle
-                {
-                    InputType = InputType.Text,
-                    DisplayName = "所属部门",
-                    IsRequired = true
-                },
-                new InputStyle
-                {
-                    InputType = InputType.Text,
-                    DisplayName = "工作地点",
-                    IsRequired = true
-                },
-                new InputStyle
-                {
-                    InputType = InputType.Text,
-                    DisplayName = "招聘人数",
-                    IsRequired = true,
-                    DefaultValue = "不限"
-                },
-                new InputStyle
-                {
-                    InputType = InputType.TextEditor,
-                    DisplayName = "工作职责",
-                    IsRequired = true
-                },
-                new InputStyle
-                {
-                    InputType = InputType.TextEditor,
-                    DisplayName = "工作要求",
-                    IsRequired = true
-                }
-            });
+                    new InputStyle
+                    {
+                        InputType = InputType.Text,
+                        DisplayName = "所属部门",
+                        IsRequired = true
+                    },
+                    new InputStyle
+                    {
+                        InputType = InputType.Text,
+                        DisplayName = "工作地点",
+                        IsRequired = true
+                    },
+                    new InputStyle
+                    {
+                        InputType = InputType.Text,
+                        DisplayName = "招聘人数",
+                        IsRequired = true,
+                        DefaultValue = "不限"
+                    },
+                    new InputStyle
+                    {
+                        InputType = InputType.TextEditor,
+                        DisplayName = "工作职责",
+                        IsRequired = true
+                    },
+                    new InputStyle
+                    {
+                        InputType = InputType.TextEditor,
+                        DisplayName = "工作要求",
+                        IsRequired = true
+                    }
+                });
         }
     }
 }
